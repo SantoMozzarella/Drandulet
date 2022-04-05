a6374ae173ec6d0489a346d9cb1c43ccad818f2a
$$$anto <102629010+SantoMozzarella@users.noreply.github.com>
2022-04-05 13:32:22 +0300

    Update send_wg.sh

--- elements/send_wg.sh
+++ elements/send_wg.sh
@@ -12,8 +12,8 @@ vpnip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 while true; do
     read -p "macOS use WireGuard at this moment? [y/n]" yn
     case $yn in
-        [Yy]* ) scp -P 25 /etc/wireguard/clients/$wgconfig administrator@$vpnip:~ ; echo "Enter macOS password" ; ssh -t administrator@$macip "sudo -s mkdir -p /usr/local/etc/wireguard && cd ~ && sudo mv ~/$wgconfig /usr/local/etc/wireguard/wg0.conf" ; read -n 1 -r -s -p $'Done, press Enter\n' ; bash ~/manage.sh ; break;;
-        [Nn]* ) read -p "Enter macOS IP:" macip ; echo -e "\n"; echo "Enter macOS password" ;  scp /etc/wireguard/clients/$wgconfig administrator@$macip:~ ; echo -e "\n"; echo "Enter macOS password's only again" ; ssh -t administrator@$macip "sudo -s mkdir -p /usr/local/etc/wireguard && cd ~ && sudo mv ~/$wgconfig /usr/local/etc/wireguard/$wgconfig" ; read -n 1 -r -s -p $'Done, press Enter\n' ; bash ~/manage.sh; exit;;
+        [Yy]* ) scp -P 25 /etc/wireguard/clients/$wgconfig administrator@$vpnip:~ ; echo "Enter macOS password" ; ssh -t administrator@$macip "sudo -s mkdir -p /usr/local/etc/wireguard && cd ~ && sudo mv ~/$wgconfig /usr/local/etc/wireguard/wg0.conf" ; read -n 1 -r -s -p $'Done, press Enter\n' ; bash ~/start.sh ; break;;
+        [Nn]* ) read -p "Enter macOS IP:" macip ; echo -e "\n"; echo "Enter macOS password" ;  scp /etc/wireguard/clients/$wgconfig administrator@$macip:~ ; echo -e "\n"; echo "Enter macOS password's only again" ; ssh -t administrator@$macip "sudo -s mkdir -p /usr/local/etc/wireguard && cd ~ && sudo mv ~/$wgconfig /usr/local/etc/wireguard/$wgconfig" ; read -n 1 -r -s -p $'Done, press Enter\n' ; bash ~/start.sh; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 
